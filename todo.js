3ed3941deef3a4d88924b9df013e4012c9d1b9a5
Nagy László Levente <nagylacika2002@gmail.com>
2023-08-08 22:48:47 +0200

    Changed class {id} to data-id

--- todo.js
+++ todo.js
@@ -5,7 +5,7 @@ function addFields() {
     const todoDate = document.querySelector("#todoDate").value;
     const todoField = document.createElement("div");
     todoField.classList.add("flexx");
-    todoField.innerHTML = `<div class="inpText ${id++}">${todoText}</div>
+    todoField.innerHTML = `<div class="inpText" data-id="${id++}">${todoText}</div>
                             <div class="inpDate">${todoDate}</div>
                             <button class="remove-todo" >Remove</button>`;
     insertTodoInOrder(todoField);
@@ -16,6 +16,7 @@ let todoArray = [];
 function insertTodoInOrder(todoElement) {
     //todoArray = JSON.parse(localStorage.getItem("items")) || [];
     //console.log(todoArray[0]);
+    //todo "??"
 
     todoArray.push(todoElement);
     todoArray.sort((a, b) => {
@@ -29,9 +30,9 @@ function insertTodoInOrder(todoElement) {
     for (let i = 0; i < todoArray.length; i++) {
         todoField.appendChild(todoArray[i]);
     }
-    console.log(todoArray);
-    localStorage.setItem("items", JSON.stringify(todoArray));
-    console.log(localStorage.getItem("items"));
+    //console.log(todoArray);
+    //localStorage.setItem("items", JSON.stringify(todoArray));
+    //console.log(localStorage.getItem("items"));
 
 
     const rmButtons = document.querySelectorAll(".remove-todo");
@@ -39,43 +40,12 @@ function insertTodoInOrder(todoElement) {
     rmButtons.forEach(item => item.addEventListener('click', event => {
         const containerDiv = event.target.parentElement;
         for (let i = 0; i < todoArray.length; i++) {
-            if (todoArray[i].children.item(0).classList.item(1) === containerDiv.children.item(0).classList.item(1)) {
+            if (todoArray[i].getAttribute("data-id") === containerDiv.getAttribute("data-id")) {
                 todoArray.splice(i, 1);
                 break;
             }
         }
         containerDiv.remove();
     }));
-
-
-    /*const existingTodos = document.querySelectorAll('.flexx');
-
-    let position = 0;
-    for (let i = existingTodos.length - 1; i >= 0; i--) {
-        const existingTodoDate = new Date(existingTodos[i].querySelector('.inpDate').innerText).getTime();
-        if (todoDate <= existingTodoDate) {
-            position = i + 1;
-            break;
-        }
-    }
-    const todoField = document.querySelector('.todo-fields');
-    if (position === existingTodos.length) {
-        todoField.appendChild(todoElement);
-    } else {
-        todoField.insertBefore(todoElement, existingTodos[position]);
-    }
-    const rmButtons = document.querySelectorAll(".remove-todo");
-    rmButtons.forEach(item => item.addEventListener('click', event => {
-            const containerDiv = event.target.parentElement;
-            containerDiv.remove();
-        }
-    ));*/
+    console.log(todoArray)
 }
-// Assuming you have a form element with id="myForm"
-const form = document.getElementById("myForm");
-
-form.addEventListener("submit", function (event) {
-    event.preventDefault(); // Prevents the default form submission and page reload behavior
-    // Your form submission handling code here
-});
-
